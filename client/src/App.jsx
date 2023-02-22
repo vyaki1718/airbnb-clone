d2a3764205251507ab2b47e361f20261f98027a4
Vivek Patil <vivekpatil1718@gmail.com>
2023-02-22 17:37:34 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 
 import {Routes, Route} from 'react-router-dom'
 import Header  from "../components/Header";
-
-
+import LoginPage from '../components/LoginPage';
+import Layout from '../components/Layout';
+import IndexPage from '../components/pages/IndexPage';
+import Register from '../components/Register';
 function App(){
 
   return(
     <Routes>
-      <Route path="/" element={<Header/>}/>
+       <Route path="/" element={<Layout/>}>
+       <Route index element={<IndexPage/>}/>
+       <Route path="/login" element={<LoginPage/>}/>
+       <Route path="/register" element={<Register/>}/>
+      </Route>
     </Routes>
      
   )
